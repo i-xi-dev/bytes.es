992a1ed2c99c10faa4f3c7a10c907dbde591dae7
i-xi-dev <naoyuki.isono@gmail.com>
2021-09-11 11:03:10 +0900

    Add test for DigestAlgorithm.register

---
+++ test/node/byte/digest_algorithm/index/register.test.js
@@ -0,0 +1,22 @@
+import assert from "node:assert";
+import crypto from "node:crypto";
+import { DigestAlgorithm } from "../../../../../node.mjs";
+
+describe("DigestAlgorithm.register", () => {
+
+  it("register(string, Object)", async () => {
+    DigestAlgorithm.register("md5", {
+      async compute(input) {
+        const md5 = crypto.createHash("md5");
+        md5.update(input);
+        return md5.digest();
+      }
+    });
+    const md5 = DigestAlgorithm.for("md5");
+
+    const md5Digest = await md5.compute(Uint8Array.of());
+    assert.strictEqual([...md5Digest].map(b=>b.toString(16).padStart(2,"0")).join(""), "d41d8cd98f00b204e9800998ecf8427e");
+
+  });
+
+});
