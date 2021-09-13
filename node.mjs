d64028b4ad47fcaeaa7a7a506854fee83d9d2c9f
i-xi-dev <naoyuki.isono@gmail.com>
2021-09-13 19:42:46 +0900

    Remove Uuid class

--- node.mjs
+++ node.mjs
@@ -5,6 +5,5 @@ globalThis.ReadableStream = (await import("node:stream/web")).ReadableStream;
 export { DigestAlgorithm } from "./dist/byte/index.js";
 export { TextEncoding } from "./dist/text_encoding/index.js";
 export { ByteSequence } from "./dist/byte_sequence.js";
-export { Uuid } from "./dist/uuid.js";
 export { MediaType } from "./dist/media_type.js";
 export { Resource } from "./dist/resource.js";
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@
 export { DigestAlgorithm, DigestAlgorithmImplementation } from "./byte/index.js";
 export { TextEncoding, TextEncodingImplementation } from "./text_encoding/index.js";
 export { ByteSequence } from "./byte_sequence.js";
-export { Uuid } from "./uuid.js";
 export { MediaType } from "./media_type.js";
 export { Resource } from "./resource.js";
