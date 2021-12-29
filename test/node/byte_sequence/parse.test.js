c76378f8eb2ce441b0896b6b320bafa4acceea25
i-xi-dev <naoyuki.isono@gmail.com>
2021-12-29 19:10:34 +0900

    0.1.0

--- test/node/byte_sequence/parse.test.js
+++ test/node/byte_sequence/parse.test.js
@@ -10,34 +10,34 @@ describe("ByteSequence.parse", () => {
     assert.throws(() => {
       ByteSequence.parse("あ");
     }, {
-      message: "parse error"
+      message: "parse error: あ"
     });
 
     assert.throws(() => {
       ByteSequence.parse("GG");
     }, {
-      message: "parse error"
+      message: "parse error: GG"
     });
 
   });
 
-  it("parse(string, number)", () => {
-    const bs0 = ByteSequence.parse("41424344", 16);
+  it("parse(string, {radix:number})", () => {
+    const bs0 = ByteSequence.parse("41424344", {radix:16});
     assert.strictEqual(bs0.toString(), "41424344");
 
-    const bs1 = ByteSequence.parse("065066067068", 10);
+    const bs1 = ByteSequence.parse("065066067068", {radix:10});
     assert.strictEqual(bs1.toString(), "41424344");
 
-    const bs2 = ByteSequence.parse("101102103104", 8);
+    const bs2 = ByteSequence.parse("101102103104", {radix:8});
     assert.strictEqual(bs2.toString(), "41424344");
 
-    const bs3 = ByteSequence.parse("01000001010000100100001101000100", 2);
+    const bs3 = ByteSequence.parse("01000001010000100100001101000100", {radix:2});
     assert.strictEqual(bs3.toString(), "41424344");
 
   });
 
-  it("parse(string, number, FormatOptions)", () => {
-    const bs0 = ByteSequence.parse("0041004200430044", 16, {paddedLength:4, upperCase:true});
+  it("parse(string, FormatOptions)", () => {
+    const bs0 = ByteSequence.parse("0041004200430044", {radix:16, paddedLength:4, upperCase:true});
     assert.strictEqual(bs0.toString(), "41424344");
 
   });
