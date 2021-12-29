f59df99c11b8be70e242c628e7350604236f3340
i-xi-dev <naoyuki.isono@gmail.com>
2021-12-29 19:18:38 +0900

    0.1.0

--- test/node/byte_sequence/format.test.js
+++ test/node/byte_sequence/format.test.js
@@ -2,25 +2,25 @@ import assert from "node:assert";
 import { ByteSequence } from "../../../node/index.mjs";
 
 describe("ByteSequence.prototype.format", () => {
-  const bs0 = ByteSequence.create(0);
+  const bs0 = ByteSequence.allocate(0);
   const bs1 = ByteSequence.of(0x41, 0x3C, 0xA, 0x20, 0xA9);
 
   it("format()", () => {
     assert.strictEqual(bs0.format(), "");
-    assert.strictEqual(bs1.format(), "413c0a20a9");
+    assert.strictEqual(bs1.format(), "413C0A20A9");
 
   });
 
-  it("format(number)", () => {
-    assert.strictEqual(bs1.format(16), "413c0a20a9");
-    assert.strictEqual(bs1.format(10), "065060010032169");
+  it("format({radix:number})", () => {
+    assert.strictEqual(bs1.format({radix:16}), "413C0A20A9");
+    assert.strictEqual(bs1.format({radix:10}), "065060010032169");
 
   });
 
-  it("format(number, FormatOptions)", () => {
-    assert.strictEqual(bs1.format(16, {upperCase:true}), "413C0A20A9");
-    assert.strictEqual(bs1.format(16, {paddedLength:3}), "04103c00a0200a9");
-    assert.strictEqual(bs1.format(10, {paddedLength:4, upperCase:true}), "00650060001000320169");
+  it("format(FormatOptions)", () => {
+    assert.strictEqual(bs1.format({radix:16,upperCase:false}), "413c0a20a9");
+    assert.strictEqual(bs1.format({radix:16,paddedLength:3,upperCase:false}), "04103c00a0200a9");
+    assert.strictEqual(bs1.format({radix:10,paddedLength:4}), "00650060001000320169");
 
   });
 
