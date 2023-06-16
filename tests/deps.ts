bff2481e68956d1727c1927eb805330abeb01699
i-xi-dev <naoyuki.isono@gmail.com>
2023-06-16 23:27:53 +0900

    4.1.2

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.190.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.192.0/testing/asserts.ts";
 export * from "https://deno.land/std@0.160.0/hash/md5.ts";
