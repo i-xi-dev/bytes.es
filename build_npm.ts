d8ca0fe97e592624a889d6347ece13fc81399ea2
i-xi-dev <naoyuki.isono@gmail.com>
2023-10-15 00:41:01 +0900

    4.1.9

--- build_npm.ts
+++ build_npm.ts
@@ -28,7 +28,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/bytes",
-    version: "4.1.8",
+    version: "4.1.9",
     description:
       "A JavaScript byte array library for the browser, Deno and Node.js",
     license: "MIT",
