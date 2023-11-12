cf8486ae02045c033b38f42dfdcfe758f9c89b4c
i-xi-dev <naoyuki.isono@gmail.com>
2023-11-12 21:30:46 +0900

    4.1.10

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.204.0/assert/mod.ts";
+export * from "https://deno.land/std@0.206.0/assert/mod.ts";
 export * from "https://deno.land/std@0.160.0/hash/md5.ts";
-export { BytesUnit } from "https://raw.githubusercontent.com/i-xi-dev/bytes-size.es/1.0.8/mod.ts";
+export { BytesUnit } from "https://raw.githubusercontent.com/i-xi-dev/bytes-size.es/1.0.9/mod.ts";
